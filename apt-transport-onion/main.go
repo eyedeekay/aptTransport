0067aa31d80ad64688c4acfa1dd4e0ec8a83ca20
idk <hankhill19580@gmail.com>
2023-03-12 20:07:46 +0000

    create demo apt-transport-onion

--- apt-transport-onion/main.go
+++ apt-transport-onion/main.go
@@ -1,9 +1,45 @@
 package main
 
-import "github.com/eyedeekay/apttransport"
+import (
+	"crypto/tls"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/eyedeekay/apttransport"
+	"github.com/eyedeekay/onramp"
+)
 
 func main() {
 	transport := &apttransport.AptMethod{}
 	transport.Main = transport.DefaultMain
-	transport.Main()
+	transport.AptString = "i2psam://"
+	onion, err := onramp.NewOnion("apt-transport-onion")
+	if err == nil {
+		samHTTPClient := GenerateSAMHTTPClient(onion)
+		transport.Client = samHTTPClient
+		transport.Main()
+	} else {
+		log.Fatal(err)
+	}
+
+}
+
+func GenerateSAMHTTPClient(onion *onramp.Onion) apttransport.AptClient {
+	aptClient := &http.Client{
+		Timeout: time.Duration(6) * time.Minute,
+		Transport: &http.Transport{
+			MaxIdleConns:          0,
+			MaxIdleConnsPerHost:   2,
+			DisableKeepAlives:     false,
+			ResponseHeaderTimeout: time.Duration(10) * time.Minute,
+			ExpectContinueTimeout: time.Duration(10) * time.Minute,
+			IdleConnTimeout:       time.Duration(10) * time.Minute,
+			TLSNextProto:          make(map[string]func(authority string, c *tls.Conn) http.RoundTripper),
+			Dial:                  onion.Dial,
+		},
+		Jar:           nil,
+		CheckRedirect: nil,
+	}
+	return aptClient
 }
