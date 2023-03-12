a76b612342c3de4fcb719b9e3bf6fd66352db016
idk <hankhill19580@gmail.com>
2023-03-12 20:03:19 +0000

    create demo apt-transport-garlic

--- Makefile
+++ Makefile
@@ -1,14 +1,18 @@
 
+CGO_ENABLED=0
+
+GOTAGS=--tags="netgo,osusergo"
+
 all: lib default onion garlic
 
 lib:
-	go build -o lib.a *.go
+	go build "${GOTAGS}" -o lib.a *.go
 
 default:
-	go build -o apt-transport-default/apt-transport-default apt-transport-default/main.go
+	go build "${GOTAGS}" -o apt-transport-default/apt-transport-default apt-transport-default/main.go
 
 onion:
-	go build -o apt-transport-onion/apt-transport-onion apt-transport-onion/main.go
+	go build "${GOTAGS}" -o apt-transport-onion/apt-transport-onion apt-transport-onion/main.go
 
 garlic:
-	go build -o apt-transport-garlic/apt-transport-garlic apt-transport-garlic/main.go
+	go build "${GOTAGS}" -o apt-transport-garlic/apt-transport-garlic apt-transport-garlic/main.go
--- apt-transport-garlic/main.go
+++ apt-transport-garlic/main.go
@@ -1,9 +1,40 @@
 package main
 
-import "github.com/eyedeekay/apttransport"
+import (
+	"crypto/tls"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/eyedeekay/apttransport"
+	"github.com/eyedeekay/onramp"
+)
 
 func main() {
 	transport := &apttransport.AptMethod{}
 	transport.Main = transport.DefaultMain
-	transport.Main()
+	transport.AptString = "i2psam://"
+	garlic, err := onramp.NewGarlic("apt-transport-garlic", "127.0.0.1:7656", nil)
+	if err == nil {
+		aptClient := &http.Client{
+			Timeout: time.Duration(6) * time.Minute,
+			Transport: &http.Transport{
+				MaxIdleConns:          0,
+				MaxIdleConnsPerHost:   2,
+				DisableKeepAlives:     false,
+				ResponseHeaderTimeout: time.Duration(10) * time.Minute,
+				ExpectContinueTimeout: time.Duration(10) * time.Minute,
+				IdleConnTimeout:       time.Duration(10) * time.Minute,
+				TLSNextProto:          make(map[string]func(authority string, c *tls.Conn) http.RoundTripper),
+				Dial:                  garlic.Dial,
+			},
+			Jar:           nil,
+			CheckRedirect: nil,
+		}
+		transport.Client = aptClient
+		transport.Main()
+	} else {
+		log.Fatal(err)
+	}
+
 }
